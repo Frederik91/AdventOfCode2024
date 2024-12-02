42b097c4c6668d18cea150ed32bc4a25fdf2c1f7
Frederik Tegnander <FRTE@COWI.Com>
2024-12-02 14:34:04 +0100

    Cleaned up code

--- Day1/PartTwo.cs
+++ Day1/PartTwo.cs
@@ -17,14 +17,7 @@ public static class PartTwo
             row1Numbers.Add(num1);
 
             var num2 = int.Parse(numbers[1]);
-            if (row2Numbers.ContainsKey(num2))
-            {
-                row2Numbers[num2]++;
-            }
-            else
-            {
-                row2Numbers[num2] = 1;
-            }
+            row2Numbers[num2] += 1;
         }
 
         var sum = 0;
--- Day1/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using System.Diagnostics;
-
-// Remove all code in this file as it will be replaced by tests
